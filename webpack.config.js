e49e5bb66df131bc7c348ff3f93d4f2b843e43d2
amba666 <ambakisyeabraham@gmail.com>
2023-11-03 15:05:00 +0300

    js

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
+const path = require('path');
+
 module.exports = {
-    module:{
+    entry: './leadmanager/frontend/src/index.js', // Entry point of your application
+    output: {
+        filename: 'main.js', // Output filename
+        path: path.resolve(__dirname, 'leadmanager/frontend/static/frontend'), // Output directory path
+    },
+    module: {
         rules: [
             {
-
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
-              }
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             }
         ]
     }
-}
+    // ... other webpack configurations if needed
+};

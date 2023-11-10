1cf7cd48c9a07447c533ddf3a9754b1770b2981d
amba666 <ambakisyeabraham@gmail.com>
2023-11-10 13:22:12 +0300

    Using Redux--HTTP-fetching data

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -9,9 +9,34 @@ export  class Form extends Component{
     }
     render(){
         return(
-            <div>
-                <h1>Add Lead Form </h1>
-            </div>
+            <div className="card card-body mt-4 mb-4">
+                <h2>Add Lead Form </h2>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input type="text" className="form-control"
+                               name="name" onChange={this.onChange} value={name}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input type="email" className="form-control"
+                               name="email" onChange={this.onChange} value={email}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Message</label>
+                        <textarea type="text" className="form-control"
+                               name="message" onChange={this.onChange} value={message}/>
+                    </div>
+                    <div className="form-group">
+
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+
+
+                </form>
+             </div>
         )
     }
 }
